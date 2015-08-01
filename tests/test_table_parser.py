a35f1d7ff15cf4ec9146d369eeea2f7ff2d1ee28
Raphael Knaus <rknuus@gmail.com>
2015-08-01 19:52:26 +0200

    properly named test

--- tests/test_table_parser.py
+++ tests/test_table_parser.py
@@ -7,7 +7,7 @@ import unittest
 
 
 class TestTableParser(unittest.TestCase):
-    def test_(self):
+    def test_get_coordinates(self):
         path = os.path.join(os.path.dirname(__file__), "sample_data/GC2A62B Seepromenade Luzern [DE_EN] (Multi-cache) "
                             "in Zentralschweiz (ZG_SZ_LU_UR_OW_NW), Switzerland created by Worlddiver.html")
         table_parser = TableParser(path)
@@ -19,15 +19,3 @@ class TestTableParser(unittest.TestCase):
                                                       '\n                ???\xa0\n\n            ',
                                                       '\n                ???\xa0\n\n            ',
                                                       '\n                ???\xa0\n\n            '])
-
-
-# TESTLIST:
-# - coordinates_in_wgs84_with_floating_point_minutes
-# - coordinates_in_wgs84_decimal
-# - coordinates_in_wgs84_with_minutes_and_seconds
-# - UTM format
-# - british grid
-# - swiss grid
-# - coordinates_in_nad27_with_floating_point_minutes
-# - coordinates_in_nad27_decimal
-# - coordinates_in_nad27_with_minutes_and_seconds
