07e7894662dc742916aa64f4cd4b6395d061b598
Raphael Knaus <rknuus@gmail.com>
2015-08-07 21:19:44 +0200

    fixed wrong uses of generators and namedtuple

--- caspr/stages.py
+++ caspr/stages.py
@@ -5,18 +5,10 @@
 from collections import namedtuple
 
 
-class Stages:
-    def __init__(self, parser):
-        self._parser = parser
+Stage = namedtuple("stage", ["fixed_coordinates"])
 
-    def __iter__(self):
-        # return self
-        for coordinates in self._parser.coordinates():
-            stage = namedtuple("stage", ["fixed_coordinates"])
-            stage.fixed_coordinates = coordinates
-            yield stage
 
-    # def next(self):
-    #     # from nose.tools import set_trace; set_trace()
-    #     # print(len(self._parser.coordinates()))
-    #     raise StopIteration()
+def stages(parser):
+    for coordinates in parser.coordinates():
+        yield Stage(fixed_coordinates=coordinates)
+    return
--- tests/test_stages.py
+++ tests/test_stages.py
@@ -2,38 +2,31 @@
 # -*- coding: utf-8 -*-
 
 
-from caspr.stages import Stages
-from caspr.tableparser import TableParser
-from collections import namedtuple
-# from testfixtures import compare
+from caspr.stages import Stage
+from caspr.stages import stages
+from testfixtures import compare
 from unittest.mock import MagicMock
-import mock  # TODO(KNR): is this module related to unittest.mock?
+from unittest.mock import Mock
 import unittest
 
 
-# def _compare_stage(x, y, context):
-#     if x.fixed_coordinates == y.fixed_coordinates:
-#         return
-#     return 'stage '
-
-def expected_generator(count):
+def _expected_stages(count):
     for i in range(count):
-        stage = namedtuple("stage", ["fixed_coordinates"])
-        stage.fixed_coordinates = str(i+1)
-        yield stage
+        yield Stage(fixed_coordinates=str(i+1))
+
+
+def _compare_stages(lhs, rhs):
+    for left, right in zip(lhs, rhs):
+        compare(left, right)
 
 
 class TestStages(unittest.TestCase):
     def test_no_stages_defined(self):
-        parser = mock.create_autospec(TableParser)
+        parser = Mock()
         parser.coordinates = MagicMock(return_value=[])
-        stages = Stages(parser)
-        with self.assertRaises(StopIteration):
-            stages.__iter__().__next__()
+        self.assertEqual(tuple(stages(parser)), ())
 
     def test_one_stage_defined(self):
-        parser = mock.create_autospec(TableParser)
+        parser = Mock()
         parser.coordinates = MagicMock(return_value=['1'])
-        stages = Stages(parser)
-        for expected_stage, actual_stage in zip(expected_generator(1), stages):
-            self.assertEqual(expected_stage.fixed_coordinates, actual_stage.fixed_coordinates)
+        _compare_stages(stages(parser), _expected_stages(1))
