35d130bb0a77367b633af8fb975897bcc032de3e
Raphael Knaus <rknuus@gmail.com>
2016-01-03 18:37:36 +0100

    support partial match of static coordinates

--- caspr/staticcoordinate.py
+++ caspr/staticcoordinate.py
@@ -6,13 +6,21 @@ class StaticCoordinate:
 
     _LONGITUDE_PATTERN = '[NS]\s*\d{1,2}[°]?\s+\d{1,2}[.]\d{3}'
     _LATTITUDE_PATTERN = '[EW]\s*\d{1,3}[°]?\s+\d{1,2}[.]\d{3}'
-    _LONGITUDE_RE = re.compile(_LONGITUDE_PATTERN)
-    _LATTITUDE_RE = re.compile(_LATTITUDE_PATTERN)
-    _COORDINATE_RE = re.compile('({longitude}\s+{lattitude})'.format(longitude=_LONGITUDE_PATTERN, lattitude=_LATTITUDE_PATTERN))
+    _PARTIAL_RE = re.compile('({longitude}|{lattitude})'.format(longitude=_LONGITUDE_PATTERN,
+                                                                lattitude=_LATTITUDE_PATTERN))
+    _COORDINATE_RE = re.compile('({longitude}\s+{lattitude})'.format(longitude=_LONGITUDE_PATTERN,
+                                                                     lattitude=_LATTITUDE_PATTERN))
 
     @staticmethod
     def match(input):
         match = re.search(StaticCoordinate._COORDINATE_RE, input)
+        if not match:
+            return None
+        return match.group()
+
+    @staticmethod
+    def match_partially(input):
+        match = re.search(StaticCoordinate._PARTIAL_RE, input)
         if not match:
             return None
         return match.group()
--- tests/test_staticcoordinate.py
+++ tests/test_staticcoordinate.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from caspr.staticcoordinate import StaticCoordinate
 import unittest
 
 
 class TestStaticCoordinate(unittest.TestCase):
     def test_coordinates_in_wgs84_with_floating_point_minutes(self):
-        inputs_outputs = {'\n                N 47° 03.204 E 008° 18.557\xa0\n\n            ': 'N 47° 03.204 E 008° 18.557'}
-        for input, output in inputs_outputs.items():
-            self.assertEquals(StaticCoordinate.match(input), output)
+        self.assertEquals(StaticCoordinate.match('\n                N 47° 03.204 E 008° 18.557\xa0\n\n            '),
+                          'N 47° 03.204 E 008° 18.557')
 
     def test_non_coordinates_yield_empty(self):
-        inputs_outputs = {'\n                ???\xa0\n\n            ': None}
-        for input, output in inputs_outputs.items():
-            self.assertEquals(StaticCoordinate.match(input), output)
+        self.assertIsNone(StaticCoordinate.match('\n                ???\xa0\n\n            '))
+
+    def test_partial_coordinates_dont_match(self):
+        self.assertIsNone(StaticCoordinate.match('N 47° 03.204'))
+        self.assertIsNone(StaticCoordinate.match('E 008° 18.557'))
+
+    def test_partial_coordinates_do_match_partially(self):
+        self.assertTrue(StaticCoordinate.match_partially('N 47° 03.204'))
+        self.assertTrue(StaticCoordinate.match_partially('E 008° 18.557'))
+
+    def test_fullcoordinates_do_match_partially_returns_longitude(self):
+        self.assertEquals(StaticCoordinate.match_partially('N 47° 03.204 E 008° 18.557'), 'N 47° 03.204')
+
 
 # TESTLIST:
 # - coordinates_in_wgs84_decimal
